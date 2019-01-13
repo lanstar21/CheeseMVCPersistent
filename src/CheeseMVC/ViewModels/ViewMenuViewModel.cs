3850cea27844348d66b76373749a6df3d581e4b0
lanstar21 <lanstar.804@gmail.com>
2019-01-12 22:28:34 -0500

    Final commit everything works

--- src/CheeseMVC/ViewModels/ViewMenuViewModel.cs
+++ src/CheeseMVC/ViewModels/ViewMenuViewModel.cs
@@ -1,10 +1,8 @@
-﻿
-using CheeseMVC.Models;
+﻿using CheeseMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CheeseMVC.Data;
 
 namespace CheeseMVC.ViewModels
 {
@@ -13,13 +11,10 @@ namespace CheeseMVC.ViewModels
         public Menu Menu { get; set; }
         public IList<CheeseMenu> Items { get; set; }
 
-        public ViewMenuViewModel(List<CheeseMenu> items, Menu newMenu)
-        { }
-
-        public ViewMenuViewModel(Menu menu, IList<CheeseMenu> items)
+        public ViewMenuViewModel(IList<CheeseMenu> cheeseMenus, Menu newMenu)
         {
-            Menu = menu;
-            Items = items;
+            this.Menu = newMenu;
+            this.Items = cheeseMenus;
         }
     }
 }
